34541d04391dd012cba771ed2ba4487169e7a403
Kamil Kulak <kulak.kam@gmail.com>
2017-02-12 17:57:20 +0100

    Storing results from Spark batch job in redis

--- spark-consumer/most_popular_airports.py
+++ spark-consumer/most_popular_airports.py
@@ -2,24 +2,27 @@
 
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming.kafka import KafkaUtils, OffsetRange
+import redis
 
-
+# utils
 def line_mapper(line):
     columns = line.split(',')
     return [(columns[17], 1), (columns[18], 1)]
 
-
 def entry_without_kafka_key(e):
     return e[1]
 
+# db access
+db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-conf = SparkConf().setAppName('IOSR-1')
+# main
+conf = SparkConf().setAppName('most_popular_airports')
 sc = SparkContext(conf=conf)
 
 rdd = KafkaUtils.createRDD(
-    sc=sc,
-    kafkaParams={'metadata.broker.list': 'localhost:9092'},
-    offsetRanges=[OffsetRange(topic='flights', partition=0, fromOffset=0, untilOffset=1936758)]
+    sc = sc,
+    kafkaParams = {'metadata.broker.list': 'localhost:9092'},
+    offsetRanges = [OffsetRange(topic = 'flights', partition = 0, fromOffset = 0, untilOffset = 1936758)]
 )
 
 lines = rdd.map(entry_without_kafka_key)
@@ -28,4 +31,4 @@ pairs = lines.flatMap(line_mapper)
 sums_by_airport = pairs.reduceByKey(lambda a, b: a + b)
 sorted = sums_by_airport.sortBy(lambda pair: pair[1], False)
 
-print sorted.take(5)
+db.set('most_popular_airports', sorted.take(5))
